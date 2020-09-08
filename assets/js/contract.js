85294c21cbf4047dc1a58583e7b3e7d9246849af
ToronDev <xmarkwilliam@yahoo.com>
2020-09-08 19:18:47 +0800

    d
    d

--- assets/js/contract.js
+++ assets/js/contract.js
@@ -23,7 +23,7 @@ function copyToClipboard(text) {
 
 function logout() {
     localStorage.removeItem('loginaddress');
-    location.href = "../";
+    location.href = "tronninja.com/login.html";
 }
 
 function getFormatedDate(unixtimestamp) {
